fc327608a90e88b15ea930ca02c1f66393340086
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-19 23:53:48 +0100

    removed echo_handler

--- app/app.py
+++ app/app.py
@@ -18,6 +18,7 @@ from memes.meme import get_random_meme
 from app.commands import START_MSG, DESC_MSG, HELP_MSG
 
 bot = Bot(config.API_KEY, parse_mode=ParseMode.HTML)
+bot.set_my_commands(["start"])
 dp = Dispatcher()
 
 
@@ -51,21 +52,6 @@ async def command_meme_handler(message: Message) -> None:
             await message.answer(text="Try one more time, the file is damaged.")
     else:
         await bot.send_photo(chat_id=message.chat.id, photo=meme)
-
-
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
 
 
 async def main() -> None:
