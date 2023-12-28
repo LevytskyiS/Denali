c6c126292551ff5bc02b9a4c7641bed2f6cf47de
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-28 16:04:57 +0100

    youtube downloader

---
+++ youtube/yt_downloader.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+from pytube import YouTube
+
+
+async def get_youtube_video(url: str) -> str:
+    download_folder = "./youtube"
+    yt = YouTube(url)
+
+    (
+        yt.streams.filter(progressive=True, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+        .download(output_path=download_folder)
+    )
+    video_path = [file for file in Path(download_folder).iterdir()][0]
+    video_size = os.path.getsize(video_path)
+
+    if video_size >= 49000000:
+        await remove_downloaded_video(video_path)
+        return
+
+    return video_path
+
+
+async def remove_downloaded_video(video_path: str) -> None:
+    try:
+        os.remove(video_path)
+    except FileNotFoundError as e:
+        print("File not found")
